445fdbd151aa2ccb4823f4760ec81c9f20e5268a
owenrumig <owenrumig@cmail.carleton.ca>
2026-01-29 11:58:07 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import About from "./components/About";
 
 export default function App() {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">
-        React is working
-      </h1>
-
+    <main>
       <About />
-    </div>
+    </main>
   );
 }
