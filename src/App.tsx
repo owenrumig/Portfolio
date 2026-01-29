50666ba2f71680f7a9b73a2029ee6821307d2d76
owenrumig <owenrumig@cmail.carleton.ca>
2026-01-29 12:09:20 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import About from "./components/About";
 import Career from "./components/Career";
 import Interests from "./components/Interests";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <main>
+      <Navbar />
+      <Hero />
       <About />
       <Interests />
       <Career />
+      <Footer />
     </main>
   );
 }
